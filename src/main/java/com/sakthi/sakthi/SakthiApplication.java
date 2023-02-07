54231a77a7088edea41a3cfbd33476b5b730e58a
sakthibazz1 <sakthiglmech123@gmail.com>
2023-02-07 17:33:10 +0530

    first commit

---
+++ src/main/java/com/sakthi/sakthi/SakthiApplication.java
@@ -0,0 +1,13 @@
+package com.sakthi.sakthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakthiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakthiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sakthi/sakthi/SakthiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakthi.sakthi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakthiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
